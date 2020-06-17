4b73c008a3f6e68a5f4ed5517db53777f8b5ea45
yoshinobc <t.980527t@gmail.com>
2020-06-18 02:41:34 +0900

    add html

--- translate/sample/views.py
+++ translate/sample/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from datetime import datetime
 # Create your views here.
+
+def index(request):
+    return HttpResponse("index !: ")
+
+def index_render(request):
+    return render(request, 'index.html')
+
+def index_dict(request):
+    d = {
+            'hour': datetime.now().hour,
+            'message': 'Sample message',
+        }
+    return render(request, 'index_dict.html',d)
